c71528b668b1ac7a82c4fb164810d9c084a083b8
nijhum_jawad2000 <njawad@techlaunch.io>
2018-12-07 20:12:51 -0500

    abc

--- exam/quiz1_php.php
+++ exam/quiz1_php.php
@@ -4,8 +4,13 @@ foreach ($numbers as $number) {
 $n=$number-1;
    if($n==0){
        $n='no more';
-       echo  '<br>'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take it down and pass it around, '.$n.' bottles of beer on the wall.';
-   }else{echo  '<br>'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottles of beer on the wall.'.'<br>';}
+       echo  '<br>'.$number.' bottle of beer on the wall, '.$number.' bottle of beer. Take it down and pass it around, '.$n.' bottles of beer on the wall.';
+   }else if($n==1){
+        $n=1;
+       echo  '<br>'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottle of beer on the wall.'.'<br>';
+    }else{
+        echo  '<br>'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottles of beer on the wall.'.'<br>';
+    }
 };
 echo '<br><br>No more bottles of beer on the wall, '. $n.' bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.';
  

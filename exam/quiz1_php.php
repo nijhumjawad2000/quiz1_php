db720b4a3b3a3ca3bf9b7b0a9515ec23a33afc11
nijhum_jawad2000 <njawad@techlaunch.io>
2018-12-08 13:14:42 -0500

    abc

--- exam/quiz1_php.php
+++ exam/quiz1_php.php
@@ -1,16 +1,22 @@
 <?php
 $numbers = range(99,1);
-foreach ($numbers as $number) {
+foreach ($numbers as $number){
 $n=$number-1;
    if($n==0){
-       $n='no more';
-       echo  '<br>'.$number.' bottle of beer on the wall, '.$number.' bottle of beer. Take it down and pass it around, '.$n.' bottles of beer on the wall.';
+       $n="no more";
+    $color ="red";
+    echo  '<br><div style ="color:'.$color.'">'.$number.' bottle of beer on the wall, '.$number.' bottle of beer. Take it down and pass it around, '.$n.' bottles of beer on the wall.</div><br>';
    }else if($n==1){
-        $n=1;
-       echo  '<br>'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottle of beer on the wall.'.'<br>';
+    $n=1;
+    $color ="black";
+    echo  '<br><div style ="color:'.$color.'">'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottle of beer on the wall.</div><br>';
+    }else if($number%2==0){
+        $color ="black";
+        echo  '<br><div style ="color:'.$color.'">'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottles of beer on the wall.</div><br>';
     }else{
-        echo  '<br>'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottles of beer on the wall.'.'<br>';
+        $color = "red";
+        echo  '<br><div style ="color:'.$color.'">'.$number.' bottles of beer on the wall, '.$number.' bottles of beer. Take one down and pass it around, '.$n.' bottles of beer on the wall.</div><br>';
     }
 };
-echo '<br><br>No more bottles of beer on the wall, '. $n.' bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.';
+echo '<br>No more bottles of beer on the wall, '. $n.' bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.';
  
